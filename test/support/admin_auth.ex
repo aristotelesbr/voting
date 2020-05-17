8bcec73307d10c3b660436a27caa64251a4f64be
Aristoteles <aristotelesbr2014@gmail.com>
2020-05-17 19:34:27 -0300

    improvement: software design

--- test/support/admin_auth.ex
+++ test/support/admin_auth.ex
@@ -11,7 +11,6 @@ defmodule VotingWeb.AdminAuth do
   def authenticate(conn, admin \\ insert(:admin)) do
     {:ok, token, _} = Guardian.encode_and_sign(admin)
 
-
     put_req_header(conn, "authorization", "Bearer " <> token)
   end
 end
